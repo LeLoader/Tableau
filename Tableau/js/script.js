cb3628c419eadb754ea4bad449c68e7c18660fb2
LeLoader <61639484+LeLoader@users.noreply.github.com>
2023-10-04 11:06:39 +0200

    Ex Tableau

---
+++ Tableau/js/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+function Tableau(){
+    let names = [];
+    names.push("Vincent");
+    names.push("Paul");
+    names.push("Arthur");
+
+    names.forEach(element => {
+        element += " va à la peche."
+        console.log(element);
+    });
+}
+
+Tableau()
